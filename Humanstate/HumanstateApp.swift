dbb2526abbfb624a6972d80222aca68cf04b0623
Noah Sadat <sadatnoah@gmail.com>
2024-07-25 12:48:01 +0200

    Initial Commit

---
+++ Humanstate/HumanstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanstateApp.swift
+//  Humanstate
+//
+//  Created by Noah Sadat on 25.07.24.
+//
+
+import SwiftUI
+
+@main
+struct HumanstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HumanstateTests/HumanstateTests.swift
@@ -0,0 +1,17 @@
+//
+//  HumanstateTests.swift
+//  HumanstateTests
+//
+//  Created by Noah Sadat on 25.07.24.
+//
+
+import Testing
+@testable import Humanstate
+
+struct HumanstateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
